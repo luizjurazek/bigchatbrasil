e4106b973b391551ef254e3717cb12b55b1cd85d
Luiz Jurazek <luizjurazek@gmail.com>
2025-02-22 13:11:33 -0300

    feat: create Error response type and client validator class

---
+++ src/main/java/com/big_chat_brasil/bigchatbrasil/dto/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.big_chat_brasil.bigchatbrasil.dto;
+
+public class ErrorResponse {
+    private String field;
+    private String error;
+
+    public ErrorResponse(String field, String error) {
+        this.field = field;
+        this.error = error;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
---
+++ src/main/java/com/big_chat_brasil/bigchatbrasil/validator/ClientValidator.java
@@ -0,0 +1,44 @@
+package com.big_chat_brasil.bigchatbrasil.validator;
+
+import com.big_chat_brasil.bigchatbrasil.dto.ErrorResponse;
+import com.big_chat_brasil.bigchatbrasil.model.Client;
+import com.big_chat_brasil.bigchatbrasil.repository.ClientRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientValidator {
+
+    private final ClientRepository clientRepository;
+
+    public ClientValidator(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<ErrorResponse> validate(Client client) {
+        List<ErrorResponse> errors = new ArrayList<>();
+
+        Client emailAlreadyInUse = clientRepository.findByEmail(client.getEmail());
+        Client cpfAlreadyInUse = clientRepository.findByCpf(client.getCpf());
+        Client cnpjAlreadyInUse = clientRepository.findByCnpj(client.getCnpj());
+        Client phoneAlreadyInUse = clientRepository.findByPhone(client.getPhone());
+
+        if (emailAlreadyInUse != null) {
+            errors.add(new ErrorResponse("email", "Email already in use"));
+        }
+        if (cpfAlreadyInUse != null) {
+            errors.add(new ErrorResponse("cpf", "CPF already in use"));
+        }
+        if (cnpjAlreadyInUse != null) {
+            errors.add(new ErrorResponse("cnpj", "CNPJ already in use"));
+        }
+        if (phoneAlreadyInUse != null) {
+            errors.add(new ErrorResponse("phone", "Phone already in use"));
+        }
+
+        return errors;
+    }
+}
